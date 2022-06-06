6de756d6ae454963a2ac3d3fa5cf9c00f3cc9a04
Patryk Woźniak <wozniakxyz@gmail.com>
2022-06-06 10:25:17 +0200

    Formating improvment, and trying to plot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requester
 import random
-# import sys
 import argparse
+import matplotlib.pyplot as plt
 
 debug = False
 
@@ -17,5 +17,15 @@ args = parser.parse_args()
 n = len(args.values)
 for i in range(n):
     incomes.append(requester.GrossToNet(args.values[i]))
-    currency = "{:,.2f} PLN".format(float(incomes[i].gross))
-    print(f"Gross: {currency}, Net: {incomes[i].net_income}")
+    # currency = "{: ,2f} PLN".format(float(incomes[i].gross))
+    print(f"Gross: {float(incomes[i].gross):,}".replace(',',' ').replace('.',',') + f" PLN, Net: {incomes[i].net_income}")
+
+y = []
+x = []
+for i in incomes:
+    # print(float(i.net_income.replace(' ','').replace('PLN','').replace(',','.')))
+    y.append(float(i.net_income.replace(' ','').replace('PLN','').replace(',','.')))
+    x.append(i.gross)
+
+plt.plot(x,y)
+plt.show()
