791926bb2cf2f6ed5f5d7e0da5bd4b3d3626193e
Patryk Woźniak <wozniakxyz@gmail.com>
2022-06-06 11:32:56 +0200

    Added argument for ploting gross over net  to argparse

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requester
 import random
 import argparse
 import matplotlib.pyplot as plt
-
+import numpy as np
 debug = False
 
 if debug:
@@ -13,6 +13,7 @@ if debug:
 incomes = []
 parser = argparse.ArgumentParser(description='Calculate net income from gross values')
 parser.add_argument('values', type=float, nargs='+', help='example: 5000.00 6000.00 7000.00 8000.00 9000.00 10000.00')
+parser.add_argument('-p','--plot', default=False, action='store_true', help='Ploting gross over net')
 args = parser.parse_args()
 n = len(args.values)
 for i in range(n):
@@ -20,12 +21,18 @@ for i in range(n):
     # currency = "{: ,2f} PLN".format(float(incomes[i].gross))
     print(f"Gross: {float(incomes[i].gross):,}".replace(',',' ').replace('.',',') + f" PLN, Net: {incomes[i].net_income}")
 
-y = []
-x = []
-for i in incomes:
-    # print(float(i.net_income.replace(' ','').replace('PLN','').replace(',','.')))
-    y.append(float(i.net_income.replace(' ','').replace('PLN','').replace(',','.')))
-    x.append(i.gross)
-
-plt.plot(x,y)
-plt.show()
+if args.plot:
+    net = []
+    gross = []
+    for i in incomes:
+        # print(float(i.net_income.replace(' ','').replace('PLN','').replace(',','.')))
+        net.append(float(i.net_income.replace(' ','').replace('PLN','').replace(',','.')))
+        gross.append(i.gross)
+    gross=np.sort(gross)
+    net=np.sort(net)
+    # points = np.arange(0,max(args.values))
+    points = np.linspace(0,max(args.values),len(args.values))
+    plt.plot(points,gross,label='Gross')
+    plt.plot(points,net,label='Net')
+    plt.legend(loc="upper left")
+    plt.show()
